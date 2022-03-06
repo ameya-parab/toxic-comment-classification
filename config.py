399c1b576973f77389746ce7fe32669ab71603d7
Ameya Parab <ameyaparab@novartis.com>
2022-03-06 17:56:20 -0500

    model configs added

--- config.py
+++ config.py
@@ -5,15 +5,22 @@ import torch
 ROOT_DIR = os.path.dirname(__file__)
 DATA_DIR = os.path.join(ROOT_DIR, "data")
 MODEL_DIR = os.path.join(ROOT_DIR, "model")
-MODEL_PATH = os.path.join(MODEL_DIR, "model.pth")
 CACHE_DIR = os.path.join(ROOT_DIR, ".huggingface_cache")
 
 os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(MODEL_DIR, exist_ok=True)
 os.makedirs(CACHE_DIR, exist_ok=True)
 
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
 STORAGE = f"sqlite:///{os.path.join(ROOT_DIR, 'study', 'hyperparameter_studies.db')}"
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
+MODEL_CHECKPOINT = "distilbert-base-uncased"
+
+METRICS = "f1_macro"
+
 MAX_LEN = 510
+
+THRESHOLD = 0.5
