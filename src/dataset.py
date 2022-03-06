a7b076c221a0f5b263672903a2e5e6fe5c65b647
Ameya Parab <ameyaparab@novartis.com>
2022-03-06 17:01:37 -0500

    implemented fetching of data

--- src/dataset.py
+++ src/dataset.py
@@ -3,51 +3,47 @@ import re
 import sys
 import typing
 
+import numpy as np
 import pandas as pd
 import torch
+from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer, DataCollatorWithPadding
 
 sys.path.insert(0, os.path.join(os.getcwd(), ".."))
 
-from config import CACHE_DIR, DATA_DIR, MAX_LEN
+from config import CACHE_DIR, DATA_DIR, MAX_LEN, MODEL_CHECKPOINT
+
+from src.utils import seed_worker, set_random_seed
 
 
 class Comments(Dataset):
-    def __init__(self, split: str, indices: typing.List[int], checkpoint: str):
+    def __init__(self, split: str, tokenizer, indices: typing.List[int] = None):
 
         self.split = split
         self.indices = indices
-        self.checkpoint = checkpoint
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            self.checkpoint, cache_dir=CACHE_DIR
-        )
-        self.data_collator = DataCollatorWithPadding(
-            tokenizer=self.tokenizer,
-            padding="longest",
-            max_length=MAX_LEN,
-            return_tensors="pt",
-        )
-        self.dataset = (
-            pd.read_csv(
-                os.path.join(
-                    DATA_DIR, "test.csv" if self.split == "test" else "train.csv"
-                )
+        self.tokenizer = tokenizer
+
+        if self.split == "train":
+            self.dataset = (
+                pd.read_csv(os.path.join(DATA_DIR, "train.csv"))
+                .iloc[self.indices]
+                .reset_index(drop=True)
             )
-            .iloc[self.indices]
-            .reset_index(drop=True)
-        )
+            self.dataset = self.dataset.assign(
+                targets=self.dataset[self.dataset.columns[2:]].values.tolist()
+            )[:200]
+        else:
+            self.dataset = pd.read_csv(os.path.join(DATA_DIR, "test.csv")).reset_index(
+                drop=True
+            )
+            self.dataset = self.dataset.assign(
+                targets=[[0, 0, 0, 0, 0, 0]] * len(self.dataset.index)
+            )[:200]
+
         self.dataset = self.dataset.assign(
             comment_text=self.dataset.comment_text.apply(lambda c: self._sanitize(c))
         )
-        if self.split == "train":
-            self.dataset = self.dataset.assign(
-                targets=self.dataset[self.dataset.columns[2:]].values.tolist()
-            )
-        else:
-            self.dataset = self.dataset.assign(
-                targets=[[0, 0, 0, 0, 0]] * (self.dataset.index + 1)
-            )
 
     def __len__(self):
 
@@ -59,15 +55,23 @@ class Comments(Dataset):
             self.dataset.iloc[idx].comment_text,
             None,
             truncation=True,
-            return_token_type_ids=True,
         )
-
-        return {
-            "ids": torch.tensor(inputs["input_ids"], dtype=torch.long),
-            "mask": torch.tensor(inputs["attention_mask"], dtype=torch.long),
-            "token_type_ids": torch.tensor(inputs["token_type_ids"], dtype=torch.long),
+        encoded_inputs = {
+            "input_ids": torch.tensor(inputs["input_ids"], dtype=torch.long),
+            "attention_mask": torch.tensor(inputs["attention_mask"], dtype=torch.long),
             "targets": torch.tensor(self.dataset.iloc[idx].targets, dtype=torch.float),
         }
+
+        if "token_type_ids" in inputs:
+            encoded_inputs.update(
+                {
+                    "token_type_ids": torch.tensor(
+                        inputs["token_type_ids"], dtype=torch.long
+                    )
+                }
+            )
+
+        return encoded_inputs
 
     @staticmethod
     def _sanitize(comment_text: str) -> str:
@@ -99,3 +103,64 @@ class Comments(Dataset):
         ).strip()  # remove words containing numbers
 
         return comment
+
+
+def fetch_dataset(random_seed: int, batch_size: int, num_workers: int = 4):
+
+    set_random_seed(random_seed)
+
+    train = pd.read_csv(os.path.join(DATA_DIR, "train.csv"))
+    train_idx, valid_idx = train_test_split(
+        np.arange(len(train.index)),
+        test_size=0.2,
+        shuffle=True,
+        random_state=random_seed,
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_CHECKPOINT, cache_dir=CACHE_DIR)
+
+    train_dataset = Comments(split="train", indices=train_idx, tokenizer=tokenizer)
+    validation_dataset = Comments(split="train", indices=valid_idx, tokenizer=tokenizer)
+
+    generator = torch.Generator()
+    generator.manual_seed(0)
+
+    data_collator = DataCollatorWithPadding(
+        tokenizer=tokenizer,
+        padding="longest",
+        max_length=MAX_LEN,
+        return_tensors="pt",
+    )
+
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        worker_init_fn=seed_worker,
+        generator=generator,
+        shuffle=True,
+        collate_fn=data_collator,
+    )
+    valid_dataloader = torch.utils.data.DataLoader(
+        validation_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        worker_init_fn=seed_worker,
+        generator=generator,
+        shuffle=False,
+        collate_fn=data_collator,
+    )
+
+    test_dataset = Comments(split="test", indices=":", tokenizer=tokenizer)
+
+    test_dataloader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        worker_init_fn=seed_worker,
+        generator=generator,
+        shuffle=False,
+        collate_fn=data_collator,
+    )
+
+    return (train_dataloader, valid_dataloader, test_dataloader)
